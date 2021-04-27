02826a6e016c7504ff5fc9bee6b6c8e71a11e529
luiz henrique <l.henrique.saraiva@gmail.com>
2021-04-27 20:14:02 -0300

    created mock soccerteams

---
+++ app/src/main/java/com/project/soccerteams/mock/MockSoccerTeams.kt
@@ -0,0 +1,30 @@
+package com.project.soccerteams.mock
+
+import com.project.soccerteams.model.SoccerTeams
+
+class MockSoccerTeams {
+
+    val listSoccerTeams = arrayListOf<SoccerTeams>(
+        SoccerTeams(1,"Atletico Madrid"),
+        SoccerTeams(2,"Real Madrid"),
+        SoccerTeams(3,"Barcelona"),
+        SoccerTeams(4,"Sevilha"),
+        SoccerTeams(5,"Real Sociedad"),
+        SoccerTeams(6,"Betis"),
+        SoccerTeams(7,"Villarreal"),
+        SoccerTeams(8,"Granada"),
+        SoccerTeams(9,"Celta de Vigo"),
+        SoccerTeams(10,"Ath. Bilbao"),
+        SoccerTeams(11,"Osasuna"),
+        SoccerTeams(12,"Levante"),
+        SoccerTeams(13,"Cadiz"),
+        SoccerTeams(14,"Valencia"),
+        SoccerTeams(15,"Getafe"),
+        SoccerTeams(16,"Alaves"),
+        SoccerTeams(17,"Elche"),
+        SoccerTeams(18,"Real Valladolid"),
+        SoccerTeams(19,"Huesca"),
+        SoccerTeams(20,"Eibar")
+    )
+
+}
