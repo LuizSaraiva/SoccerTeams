e6baa7961c4609db95ead374a4684e4117d11b13
luiz henrique <l.henrique.saraiva@gmail.com>
2021-04-28 19:42:40 -0300

    created singletonchampionship

---
+++ app/src/main/java/com/project/soccerteams/SingletonChampionship.kt
@@ -0,0 +1,7 @@
+package com.project.soccerteams
+
+import com.project.soccerteams.model.Championship
+
+object SingletonChampionship {
+        var list: MutableList<Championship> = mutableListOf()
+}
