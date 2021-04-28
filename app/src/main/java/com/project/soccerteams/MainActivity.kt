aa4d4efcac03f771c04c84ae834a2077a6f3ab49
luiz henrique <l.henrique.saraiva@gmail.com>
2021-04-28 19:35:09 -0300

    added fragment to the main activity

--- app/src/main/java/com/project/soccerteams/MainActivity.kt
+++ app/src/main/java/com/project/soccerteams/MainActivity.kt
@@ -2,10 +2,41 @@ package com.project.soccerteams
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.project.soccerteams.mock.MockChampionship
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var navBottomMain: BottomNavigationView
+    lateinit var fragmentFirst: FragmentFirst
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MockChampionship.getData()
+
+        navBottomMain = findViewById(R.id.nav_main)
+        navBottomMain.setOnNavigationItemSelectedListener(this)
+
+        fragmentFirst = FragmentFirst()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frag_main,fragmentFirst)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.itemEurope -> {
+
+            }
+        }
+        return true
+    }
+
 }
