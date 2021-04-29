e34231ac565a00d6d2583e7bdcaa730c1b7d8f08
luiz henrique <l.henrique.saraiva@gmail.com>
2021-04-28 21:00:13 -0300

    added second championship, world cup

--- app/src/main/java/com/project/soccerteams/mock/MockChampionship.kt
+++ app/src/main/java/com/project/soccerteams/mock/MockChampionship.kt
@@ -13,7 +13,13 @@ object MockChampionship {
             Championship(
                 1,
                 "UEFA",
-                arrayListOf<SoccerTeams>(
+                arrayListOf(
+                    SoccerTeams(1, "Atletico Madrid"),
+                    SoccerTeams(2, "Real Madrid"),
+                    SoccerTeams(3, "Barcelona"),
+                    SoccerTeams(4, "Sevilha"),
+                    SoccerTeams(5, "Real Sociedad"),
+                    SoccerTeams(6, "Betis"),
                     SoccerTeams(7, "Villarreal"),
                     SoccerTeams(8, "Granada"),
                     SoccerTeams(9, "Celta de Vigo"),
@@ -30,7 +36,48 @@ object MockChampionship {
                     SoccerTeams(20, "Eibar")
                 )
             )
-
         )
+
+        SingletonChampionship.list.add(
+            Championship(
+                2,
+                "WorldCup",
+                arrayListOf(
+                    SoccerTeams(1, "Russia"),
+                    SoccerTeams(2, "Alemanha"),
+                    SoccerTeams(3, "Brasil"),
+                    SoccerTeams(4, "Portugal"),
+                    SoccerTeams(5, "Argentina"),
+                    SoccerTeams(6, "Belgica"),
+                    SoccerTeams(7, "Polonia"),
+                    SoccerTeams(8, "França"),
+                    SoccerTeams(9, "Espanha"),
+                    SoccerTeams(10, "Peru"),
+                    SoccerTeams(11, "Suiça"),
+                    SoccerTeams(12, "Inglaterra"),
+                    SoccerTeams(13, "Colombia"),
+                    SoccerTeams(14, "Mexico"),
+                    SoccerTeams(15, "Uruguai"),
+                    SoccerTeams(16, "Croacia"),
+                    SoccerTeams(17, "Dinamarca"),
+                    SoccerTeams(18, "Islandia"),
+                    SoccerTeams(19, "Costa Rica"),
+                    SoccerTeams(20, "Suecia"),
+                    SoccerTeams(21, "Tunisia"),
+                    SoccerTeams(22, "Egito"),
+                    SoccerTeams(23, "Senegal"),
+                    SoccerTeams(24, "Ira"),
+                    SoccerTeams(25, "Servia"),
+                    SoccerTeams(26, "Nigeria"),
+                    SoccerTeams(27, "Australia"),
+                    SoccerTeams(28, "Japao"),
+                    SoccerTeams(29, "Marrocos"),
+                    SoccerTeams(30, "Panama"),
+                    SoccerTeams(31, "Coreia do Sul"),
+                    SoccerTeams(31, "Arabia Saudita"),
+                )
+            )
+        )
+
     }
 }
