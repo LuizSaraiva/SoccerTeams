c9953a071ea0ad0a930b84a3d763ff403063a3b7
luiz henrique <l.henrique.saraiva@gmail.com>
2021-04-29 19:51:07 -0300

    added adapter click event

--- app/src/main/java/com/project/soccerteams/view/FragmentSecondAdapter.kt
+++ app/src/main/java/com/project/soccerteams/view/FragmentSecondAdapter.kt
@@ -1,16 +1,23 @@
 package com.project.soccerteams.view
 
+import android.graphics.drawable.Drawable
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.project.soccerteams.R
 import com.project.soccerteams.model.Championship
 
 class FragmentSecondAdapter(
     val idItemBottomView: Int,
-    val listChampionship: List<Championship>
+    val listChampionship: List<Championship>,
+    private val onClick: ((Int) -> Unit)
 ) :
     RecyclerView.Adapter<ViewHolderSecond>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderSecond {
@@ -21,7 +28,10 @@ class FragmentSecondAdapter(
 
     override fun onBindViewHolder(holder: ViewHolderSecond, position: Int) {
         var championship = listChampionship[idItemBottomView].listSoccerTeams[position]
+
         holder.txName.text = championship.name
+        holder.image.load(championship.image)
+        holder.layout.setOnClickListener { onClick(championship.id) }
     }
 
     override fun getItemCount(): Int = listChampionship[idItemBottomView].listSoccerTeams.size
@@ -29,6 +39,7 @@ class FragmentSecondAdapter(
 
 class ViewHolderSecond(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    var txName = itemView.findViewById<TextView>(R.id.name_team)
-
+    val image = itemView.findViewById<ImageView>(R.id.image_item)
+    val txName = itemView.findViewById<TextView>(R.id.name_team)
+    val layout = itemView.findViewById<CardView>(R.id.layoutItem)
 }
