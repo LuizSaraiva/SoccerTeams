0746fc63a1edf1be6b846c316c77fc2b06ec69ed
luiz henrique <l.henrique.saraiva@gmail.com>
2021-04-29 19:52:04 -0300

    update in the mock, decreasing the number of teams

--- app/src/main/java/com/project/soccerteams/mock/MockChampionship.kt
+++ app/src/main/java/com/project/soccerteams/mock/MockChampionship.kt
@@ -1,5 +1,6 @@
 package com.project.soccerteams.mock
 
+import com.project.soccerteams.R
 import com.project.soccerteams.singleton.SingletonChampionship
 import com.project.soccerteams.model.Championship
 import com.project.soccerteams.model.SoccerTeams
@@ -14,26 +15,16 @@ object MockChampionship {
                 1,
                 "UEFA",
                 arrayListOf(
-                    SoccerTeams(1, "Atletico Madrid"),
-                    SoccerTeams(2, "Real Madrid"),
-                    SoccerTeams(3, "Barcelona"),
-                    SoccerTeams(4, "Sevilha"),
-                    SoccerTeams(5, "Real Sociedad"),
-                    SoccerTeams(6, "Betis"),
-                    SoccerTeams(7, "Villarreal"),
-                    SoccerTeams(8, "Granada"),
-                    SoccerTeams(9, "Celta de Vigo"),
-                    SoccerTeams(10, "Ath. Bilbao"),
-                    SoccerTeams(11, "Osasuna"),
-                    SoccerTeams(12, "Levante"),
-                    SoccerTeams(13, "Cadiz"),
-                    SoccerTeams(14, "Valencia"),
-                    SoccerTeams(15, "Getafe"),
-                    SoccerTeams(16, "Alaves"),
-                    SoccerTeams(17, "Elche"),
-                    SoccerTeams(18, "Real Valladolid"),
-                    SoccerTeams(19, "Huesca"),
-                    SoccerTeams(20, "Eibar")
+                    SoccerTeams(1, "Atletico Madrid", 0),
+                    SoccerTeams(2, "Real Madrid", 0),
+                    SoccerTeams(3, "Barcelona", 0),
+                    SoccerTeams(4, "Sevilha", 0),
+                    SoccerTeams(5, "Real Sociedad", 0),
+                    SoccerTeams(6, "Betis", 0),
+                    SoccerTeams(7, "Villarreal", 0),
+                    SoccerTeams(8, "Granada", 0),
+                    SoccerTeams(9, "Celta de Vigo", 0),
+                    SoccerTeams(10, "Ath. Bilbao", 0)
                 )
             )
         )
@@ -43,38 +34,16 @@ object MockChampionship {
                 2,
                 "WorldCup",
                 arrayListOf(
-                    SoccerTeams(1, "Russia"),
-                    SoccerTeams(2, "Alemanha"),
-                    SoccerTeams(3, "Brasil"),
-                    SoccerTeams(4, "Portugal"),
-                    SoccerTeams(5, "Argentina"),
-                    SoccerTeams(6, "Belgica"),
-                    SoccerTeams(7, "Polonia"),
-                    SoccerTeams(8, "França"),
-                    SoccerTeams(9, "Espanha"),
-                    SoccerTeams(10, "Peru"),
-                    SoccerTeams(11, "Suiça"),
-                    SoccerTeams(12, "Inglaterra"),
-                    SoccerTeams(13, "Colombia"),
-                    SoccerTeams(14, "Mexico"),
-                    SoccerTeams(15, "Uruguai"),
-                    SoccerTeams(16, "Croacia"),
-                    SoccerTeams(17, "Dinamarca"),
-                    SoccerTeams(18, "Islandia"),
-                    SoccerTeams(19, "Costa Rica"),
-                    SoccerTeams(20, "Suecia"),
-                    SoccerTeams(21, "Tunisia"),
-                    SoccerTeams(22, "Egito"),
-                    SoccerTeams(23, "Senegal"),
-                    SoccerTeams(24, "Ira"),
-                    SoccerTeams(25, "Servia"),
-                    SoccerTeams(26, "Nigeria"),
-                    SoccerTeams(27, "Australia"),
-                    SoccerTeams(28, "Japao"),
-                    SoccerTeams(29, "Marrocos"),
-                    SoccerTeams(30, "Panama"),
-                    SoccerTeams(31, "Coreia do Sul"),
-                    SoccerTeams(31, "Arabia Saudita"),
+                    SoccerTeams(1, "Russia", R.drawable.russia),
+                    SoccerTeams(2, "Alemanha", R.drawable.germany),
+                    SoccerTeams(3, "Brasil", R.drawable.brazil),
+                    SoccerTeams(4, "Portugal", R.drawable.portugal),
+                    SoccerTeams(5, "Argentina", R.drawable.argentina),
+                    SoccerTeams(6, "Belgica", R.drawable.belgium),
+                    SoccerTeams(7, "Polonia", R.drawable.republic_of_poland),
+                    SoccerTeams(8, "França", R.drawable.france),
+                    SoccerTeams(9, "Espanha", R.drawable.spain),
+                    SoccerTeams(10, "Peru", R.drawable.peru)
                 )
             )
         )
