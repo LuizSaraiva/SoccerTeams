95f32ab31b3f9a099d3226eb3549736a4e14886f
luiz henrique <l.henrique.saraiva@gmail.com>
2021-04-29 21:10:00 -0300

    added description to the teams

--- app/src/main/java/com/project/soccerteams/mock/MockChampionship.kt
+++ app/src/main/java/com/project/soccerteams/mock/MockChampionship.kt
@@ -15,16 +15,16 @@ object MockChampionship {
                 1,
                 "UEFA",
                 arrayListOf(
-                    SoccerTeams(1, "Atletico Madrid", 0),
-                    SoccerTeams(2, "Real Madrid", 0),
-                    SoccerTeams(3, "Barcelona", 0),
-                    SoccerTeams(4, "Sevilha", 0),
-                    SoccerTeams(5, "Real Sociedad", 0),
-                    SoccerTeams(6, "Betis", 0),
-                    SoccerTeams(7, "Villarreal", 0),
-                    SoccerTeams(8, "Granada", 0),
-                    SoccerTeams(9, "Celta de Vigo", 0),
-                    SoccerTeams(10, "Ath. Bilbao", 0)
+                    SoccerTeams(1, "Atletico Madrid", 0, ""),
+                    SoccerTeams(2, "Real Madrid", 0, ""),
+                    SoccerTeams(3, "Barcelona", 0, ""),
+                    SoccerTeams(4, "Sevilha", 0, ""),
+                    SoccerTeams(5, "Real Sociedad", 0, ""),
+                    SoccerTeams(6, "Betis", 0, ""),
+                    SoccerTeams(7, "Villarreal", 0, ""),
+                    SoccerTeams(8, "Granada", 0, ""),
+                    SoccerTeams(9, "Celta de Vigo", 0, ""),
+                    SoccerTeams(10, "Ath. Bilbao", 0, "")
                 )
             )
         )
@@ -34,19 +34,25 @@ object MockChampionship {
                 2,
                 "WorldCup",
                 arrayListOf(
-                    SoccerTeams(1, "Russia", R.drawable.russia),
-                    SoccerTeams(2, "Alemanha", R.drawable.germany),
-                    SoccerTeams(3, "Brasil", R.drawable.brazil),
-                    SoccerTeams(4, "Portugal", R.drawable.portugal),
-                    SoccerTeams(5, "Argentina", R.drawable.argentina),
-                    SoccerTeams(6, "Belgica", R.drawable.belgium),
-                    SoccerTeams(7, "Polonia", R.drawable.republic_of_poland),
-                    SoccerTeams(8, "França", R.drawable.france),
-                    SoccerTeams(9, "Espanha", R.drawable.spain),
-                    SoccerTeams(10, "Peru", R.drawable.peru)
+                    SoccerTeams(
+                        1, "Russia", R.drawable.russia,
+                        "A Seleção Russa de Futebol representa a Rússia nas competições de futebol da FIFA. " +
+                                "Surgiu após a dissolução da Seleção da CEI, criada apenas para disputar a Eurocopa de 1992, " +
+                                "para a qual a seleção da URSS, extinta juntamente com o país no ano anterior, já estava classificada"
+                    ),
+                    SoccerTeams(2, "Alemanha", R.drawable.germany,
+                    "A Seleção Alemã de Futebol, é organizada pela Federação Alemã de Futebol, instituição que administra o " +
+                            "futebol na Alemanha, e representa o país nas competições de futebol da UEFA e FIFA."),
+                    SoccerTeams(3, "Brasil", R.drawable.brazil,""),
+                    SoccerTeams(4, "Portugal", R.drawable.portugal,""),
+                    SoccerTeams(5, "Argentina", R.drawable.argentina,""),
+                    SoccerTeams(6, "Belgica", R.drawable.belgium,""),
+                    SoccerTeams(7, "Polonia", R.drawable.republic_of_poland,""),
+                    SoccerTeams(8, "França", R.drawable.france,""),
+                    SoccerTeams(9, "Espanha", R.drawable.spain,""),
+                    SoccerTeams(10, "Peru", R.drawable.peru,"")
                 )
             )
         )
-
     }
 }
