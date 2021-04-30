8ae9a4dc552a1bac1c9b35567801d0eb2eae87f2
luiz henrique <l.henrique.saraiva@gmail.com>
2021-04-30 20:30:12 -0300

    added event click

--- app/src/main/java/com/project/soccerteams/view/FragmentFirst.kt
+++ app/src/main/java/com/project/soccerteams/view/FragmentFirst.kt
@@ -1,5 +1,7 @@
 package com.project.soccerteams.view
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,14 +9,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.project.soccerteams.ActivityDestailsTeams
 import com.project.soccerteams.FragmentFirstAdapter
 import com.project.soccerteams.R
 import com.project.soccerteams.singleton.SingletonChampionship
 
 class FragmentFirst : Fragment() {
 
-    lateinit var rv_first : RecyclerView
-    val idItemBottomView:Int = 0
+    lateinit var rv_first: RecyclerView
+    val idItemBottomView: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,14 +26,22 @@ class FragmentFirst : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_first, container, false)
 
-        val adapter = FragmentFirstAdapter(idItemBottomView, SingletonChampionship.list)
-
         rv_first = view.findViewById(R.id.rv_first)
         var layoutManager = LinearLayoutManager(view.context)
         rv_first.layoutManager = layoutManager
-        rv_first.adapter = adapter
+        rv_first.adapter = FragmentFirstAdapter(
+            idItemBottomView,
+            SingletonChampionship.list
+        ) { onClickItemRecycleView(view.context, 0, it) }
 
         return view
-
     }
+
+    private fun onClickItemRecycleView(context: Context, idChampionship: Int, idTeam: Int) {
+        var intent = Intent(context, ActivityDestailsTeams::class.java)
+            .putExtra("idChamphionship", idChampionship)
+            .putExtra("idTeam", idTeam)
+        startActivity(intent)
+    }
+
 }
--- app/src/main/java/com/project/soccerteams/view/FragmentFirstAdapter.kt
+++ app/src/main/java/com/project/soccerteams/view/FragmentFirstAdapter.kt
@@ -1,17 +1,22 @@
 package com.project.soccerteams
 
+import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.project.soccerteams.model.Championship
 
 class FragmentFirstAdapter(
     val idItemBottomView: Int,
-    val listChampionship: List<Championship>
+    val listChampionship: List<Championship>,
+    private val onClick: ((Int) -> Unit)
 ) :
     RecyclerView.Adapter<ViewHolderFirst>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderFirst {
@@ -23,6 +28,12 @@ class FragmentFirstAdapter(
     override fun onBindViewHolder(holder: ViewHolderFirst, position: Int) {
         var championship = listChampionship[idItemBottomView].listSoccerTeams[position]
         holder.txName.text = championship.name
+        holder.image.load(championship.image) {
+            placeholder(R.drawable.ic_image)
+            fallback(R.drawable.ic_image)
+            error(R.drawable.ic_image)
+        }
+        holder.layout.setOnClickListener { onClick(championship.id) }
     }
 
     override fun getItemCount(): Int = listChampionship[idItemBottomView].listSoccerTeams.size
@@ -31,5 +42,7 @@ class FragmentFirstAdapter(
 class ViewHolderFirst(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     var txName = itemView.findViewById<TextView>(R.id.name_team)
+    var image = itemView.findViewById<ImageView>(R.id.image_item)
+    var layout = itemView.findViewById<CardView>(R.id.layoutItem)
 
 }
